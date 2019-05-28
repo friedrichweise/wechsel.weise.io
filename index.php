4c30d793116b0453fa65ba720cdf90d9e7c855bc
friedrichweise <account@weise.io>
2019-05-28 12:39:44 +0200

    new download buttons

--- index.php
+++ index.php
@@ -21,14 +21,22 @@
 			<h2>keyboard focused utility to connect bluetooth audio devices to your Mac</h2>
 		</header>
 		<p>wechsel (/ˈvɛksəl/) claims to simplify the interaction with bluetooth connections on macOS. Instead of using the builtin Bluetooth menu bar, seamlessly switch between bluetooth devices with your keyboard. The tool offers a global hotkey to display a Spotlight-like window. After selecting a specific device using the arrow keys, simply hit the ↵ Enter key to connect/disconnect the device.<br>
-		The <a href="https://github.com/friedrichweise/wechsel/">open-source</a> utility is written in Swift 5.0 and available for free.</p>
 		<?php 
 			ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)'); 
 			$apiURL = "https://api.github.com/repos/friedrichweise/wechsel/releases";
 			$content = json_decode(file_get_contents($apiURL), true);
 			if($content): ?>
 		<div class="downloadSection">
-			<a href="<?php echo($content[0]['assets'][0]['browser_download_url']); ?>" class="downloadButton">Download wechsel<br><span class="version"><?php echo($content[0]['tag_name']); ?></span></a>
+			<a href="<?php echo($content[0]['assets'][0]['browser_download_url']); ?>" class="downloadButton">
+				<span class="icon icon--download"></span>
+				Download
+				<span class="light"><?php echo($content[0]['tag_name']); ?></span>
+			</a>
+			<a href="https://github.com/friedrichweise/wechsel/" class="downloadButton">
+				<span class="icon icon--github"></span>
+				Github
+				<span class="light">GPLv3</span>
+			</a>
 			<p class="meta">
 				Last Updated: <?php $d = new DateTime($content[0]['published_at']); echo $d->format('d.m.Y'); ?>, <a href="<?php echo($content[0]['html_url']); ?>">Changelog</a>
 			</p>
