a359e18da286b97af9d0a3af424e56555e47d33f
friedrichweise <account@weise.io>
2023-06-09 11:44:01 +0200

    update deprecated brew cask command

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 			<div class="homebrew">
 				<b>Homebrew Installation</b>
 				<pre>brew tap friedrichweise/wechsel
-brew cask install wechsel</pre>
+brew install wechsel</pre>
 			</div>
 		</div>
 		<?php endif; ?>
